57e250426838dcf3df9e588852ee60a3cb639388
WTAlmy <walmy@ucsc.edu>
2021-02-01 23:50:04 -0800

    Prototype Finished

--- clutter.py
+++ clutter.py
@@ -1,5 +1,6 @@
 import os
 import time
+import ntpath
 import platform
 import subprocess
 
@@ -7,13 +8,15 @@ from os import listdir
 from pathlib import Path
 from datetime import datetime
 
-# Default: Two Million Seconds (3.30 Weeks)
-RECENT = 2000000
+# Default: Two Million Seconds (2.48 Weeks)
+RECENT = 1200000
+CLUTTER_NAME = "Clutter"
 locations = ["Downloads", "Desktop"]
 
 # Command String
 date_format = "%Y-%m-%d %H:%M:%S %z"
 mac_added_template = ["mdls", "-name", "kMDItemDateAdded", "-raw"]
+
 
 def mac_date_added(file_path):
   command = mac_added_template.copy()
@@ -25,6 +28,7 @@ def mac_date_added(file_path):
   except ValueError:
     return time.time()
 
+
 def recently_used(file_path):
   most_recent = 0
   recent_time = time.time() - RECENT
@@ -35,17 +39,36 @@ def recently_used(file_path):
     return True
   if platform.system() == "Darwin": # Mac OS
     return mac_date_added(file_path) > recent_time
-  return True
+  return False
+
+
+def archive(source_folder, files_list):
+  clutter_path = os.path.join(source_folder, CLUTTER_NAME)
+  if not os.path.exists(clutter_path):
+    os.makedirs(clutter_path)
+  if os.path.isdir(clutter_path):
+    for f_path in files_list:
+      try:
+        print("-- ARCHIVING: " + f_path[:65])
+        f_name = ntpath.basename(f_path)
+        os.rename(f_path, os.path.join(clutter_path, f_name))
+      except OSError as e:
+        print("OSError: " + f_path)
+        print(e)
 
 def evaluate(folder):
   folder_path = str(Path.home() / folder)
+  unused_items = []
   for f in listdir(folder_path):
-    #print("  " + folder + "/" + f[:50])
     file_path = os.path.join(folder_path, f)
     if not recently_used(file_path):
-      print("---- ARCHIVING: " + file_path[:60])
-    #else:
-      #print("++++ USED RECENTLY")
+      if os.path.isfile(file_path):
+          unused_items.append(file_path)
+  if (len(unused_items) > 0):
+    archive(folder_path, unused_items)
+  else:
+    print("-- All clean")
+
 
 for folder in locations:
   print("Checking Folder: " + folder)
